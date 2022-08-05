866c640fed75659e803ffe32b73fc549e254fc54
thiakovidis <thiakovidis@agroapps.gr>
2022-08-05 18:48:56 +0300

    Added dummy templates

--- projects/views.py
+++ projects/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return HttpResponse("Here is our projects")
+    return render(request, "projects/projects.html")
 
 
 def project(request, pk):
-    return HttpResponse("SINGLE PROJECT" + " " + str(pk))
+    return render(request, "projects/single-project.html")
