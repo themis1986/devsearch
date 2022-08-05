1a7e72ac907619dd6c178384cd2a9554b60c5efa
thiakovidis <thiakovidis@agroapps.gr>
2022-08-05 18:22:47 +0300

    Added basic url routes examples

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'ptojects.apps.ProjectsConfig',
+    'projects.apps.ProjectsConfig',
 ]
 
 MIDDLEWARE = [
