68fefe77048fa414e15b5f7729d11e5055d2e82f
thiakovidis <thiakovidis@agroapps.gr>
2022-08-06 19:28:44 +0300

    Added CRUD opearations using django ModelForm

--- projects/views.py
+++ projects/views.py
@@ -1,24 +1,6 @@
-from django.forms import NumberInput
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Project
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work'
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community'
-    }
-]
+from .forms import ProjectForm
 
 
 def projects(request):
@@ -41,3 +23,41 @@ def project(request, pk):
     }
 
     return render(request, "projects/single-project.html", context)
+
+
+def createProject(request):
+    form = ProjectForm()
+
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("projects")
+
+    context = {"form": form}
+    return render(request, "projects/project_form.html", context)
+
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == "POST":
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect("projects")
+
+    context = {"form": form}
+    return render(request, "projects/project_form.html", context)
+
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+
+    if request.method == "POST":
+        project.delete()
+        return redirect("projects")
+
+    context = {"object": project}
+    return render(request, "projects/delete_template.html", context)
