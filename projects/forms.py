b5553481f48b64f8dbfeeeef9945e181dca7e5b9
thiakovidis <thiakovidis@agroapps.gr>
2022-08-12 16:29:20 +0300

    Added UI template for creating new projects form

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 
@@ -8,3 +9,20 @@ class ProjectForm(ModelForm):
         # fields = "__all__"
         fields = ["title", "featured_image", "description",
                   "demo_link", "source_link", "tags"]
+
+        widgets = {
+            "tags": forms.CheckboxSelectMultiple()
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        print(self.fields.items())
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({"class": "input"})
+
+        # self.fields["title"].widget.attrs.update(
+        #     {"class": "input"})
+
+        # self.fields["description"].widget.attrs.update(
+        #     {"class": "input"})
