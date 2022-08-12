e436418130add65403101c2547c24888367fd1cf
thiakovidis <thiakovidis@agroapps.gr>
2022-08-12 17:29:12 +0300

    Created users model / added relationship(One to Many)  with django User model

--- projects/models.py
+++ projects/models.py
@@ -1,14 +1,17 @@
-from xmlrpc.client import TRANSPORT_ERROR
 from django.db import models
 import uuid
+from users.models import Profile
 
 # Create your models here.
 
 
 class Project(models.Model):
+    owner = models.ForeignKey(
+        Profile, null=True, blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    featured_image = models.ImageField(null=True, blank=True, default="default.jpg")
+    featured_image = models.ImageField(
+        null=True, blank=True, default="default.jpg")
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
     tags = models.ManyToManyField("Tag", blank=True)
