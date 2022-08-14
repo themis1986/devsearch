e2840fb4ff598f7de7cf51d77bbe5d51afff2ed8
thiakovidis <thiakovidis@agroapps.gr>
2022-08-14 16:30:03 +0300

    Created UI forms for login and signup

--- users/forms.py
+++ users/forms.py
@@ -11,3 +11,10 @@ class CustomUserCreationForm(UserCreationForm):
         labels = {
             "first_name": "Name"
         }
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        print(self.fields.items())
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({"class": "input"})
